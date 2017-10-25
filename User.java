215966a64b74bb764099d4401ce8a52a606e9b89
rishabh19982 <bansalrishabh1998@gmail.com>
2017-10-25 10:27:21 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,77 @@
+package Daily1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class User{
+	Scanner ip=new Scanner(System.in);
+	static Scanner ssc=new Scanner(System.in);
+	private static String name;
+	private static String password;
+	private String description;
+	private ArrayList<Expense> expense_list;
+	private ArrayList<Income> income_list;
+	
+	public User(String name, String password){
+		this.name = name;
+		this.password = password;
+		this.expense_list = new ArrayList();
+		this.income_list  = new ArrayList();
+	}
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static String getName(){
+		name=ssc.next();
+		return name;
+	}
+	
+	private static String getPassword(){
+		password=ssc.next();
+		return password;
+	}
+
+	public ArrayList<Expense> getExpenses(){
+		System.out.println("Enter description...");
+		description=ip.next();
+		Expense expense=new Expense(description+"");
+		expense_list.add(expense);
+		return this.expense_list;
+	}
+	
+	public ArrayList<Income> getIncomes(){
+		System.out.println("Enter description...");
+		description=ip.next();
+		Income income=new Income(description+"");
+		income_list.add(income);
+		return this.income_list;
+	}
+	public static void main(String[] args)
+	{
+		User u=new User();
+		Scanner sc=new Scanner(System.in);
+		int opt=4;
+		while(opt!=3)
+		{
+		System.out.println("1.INCOME \n2.EXPENSE \n3.EXIT");
+		while(opt>3)
+		{
+			System.out.println("Enter a number between 1 and 3");
+			opt=sc.nextInt();
+		}
+		switch(opt)
+		{
+		case 1:
+			u.getIncomes();
+			break;
+		case 2:
+			u.getExpenses();
+			break;
+		default:
+			
+		}
+		}
+	}
+}
