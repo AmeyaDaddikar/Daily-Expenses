7342ab9f06360d19159f77a93016425fa94af943
Rajkhandor <khandorraj@gmail.com>
2017-11-12 12:35:47 +0530

    Add files via upload
    Download the jfreechart jar file from http://www.jfree.org/jfreechart/download.html
    Add it to the project : 
    https://stackoverflow.com/questions/3280353/how-to-import-a-jar-in-eclipse

---
+++ LineGraph.java
@@ -0,0 +1,70 @@
+package dailyExpenses;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+class JavaJFreeChartLineChartExample extends ApplicationFrame {
+
+	public JavaJFreeChartLineChartExample(String title) {
+		super(title);
+		CategoryDataset dataset = createDataset();
+		JFreeChart chart = createChart(dataset,title);
+		ChartPanel chartPanel = new ChartPanel(chart);
+		chartPanel.setPreferredSize(new Dimension(500, 270));
+		setContentPane(chartPanel);
+	}
+	public static void getDataSet(ArrayList<String> date,ArrayList<Integer> amount)
+	{
+		//Get amount of money the user has when a transaction is made and store it in amount
+		//Get the date when a transaction is made 
+		date.add("14/12/1998");
+		date.add("14/12/2016");
+		date.add("14/12/2017");
+		amount.add(10);
+		amount.add(100);
+		amount.add(1000);
+	}
+	private static CategoryDataset createDataset() {
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		ArrayList<String> date = new ArrayList<String>();
+		ArrayList<Integer> amount = new ArrayList<Integer>();
+		getDataSet(date,amount);
+		for( int i=0 ; i<amount.size() ; i++ )
+		{
+			dataset.addValue(amount.get(i),"Classes",date.get(i));
+		}
+		return dataset;
+	}
+
+	private static JFreeChart createChart(CategoryDataset dataset,String title) {
+		
+		JFreeChart chart = ChartFactory.createLineChart("Daily Expenses","Months", title,dataset,PlotOrientation.VERTICAL,false, true, false);
+		
+		chart.setBackgroundPaint(Color.white);
+		CategoryPlot plot = (CategoryPlot) chart.getPlot();
+		plot.setBackgroundPaint(Color.lightGray);
+		plot.setRangeGridlinePaint(Color.white);
+		
+		NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+		rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+		
+		LineAndShapeRenderer renderer = (LineAndShapeRenderer) plot.getRenderer();
+		renderer.setShapesVisible(true);
+		renderer.setDrawOutlines(true);
+		renderer.setUseFillPaint(true);
+		renderer.setFillPaint(Color.white);
+		return chart;
+	}
+
+}
---
+++ PieChart.java
@@ -0,0 +1,53 @@
+package dailyExpenses;
+
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+import org.jfree.ui.ApplicationFrame;
+ 
+public class PieChart extends ApplicationFrame {
+   
+   public PieChart( String title ) {
+      super( title ); 
+      setContentPane(createDemoPanel( ));
+   }
+   public static void getDataSet(ArrayList<String> category,ArrayList<Integer> amount)
+	{
+	   //Get Expenses from the database as per category
+		category.add("Food");
+		category.add("Education");
+		category.add("Party");
+		amount.add(100);
+		amount.add(500);
+		amount.add(100);
+	}
+   private static PieDataset createDataset( )
+   {
+      DefaultPieDataset dataset = new DefaultPieDataset( );ArrayList<String> date = new ArrayList<String>();
+	  ArrayList<Integer> amount = new ArrayList<Integer>();
+	  getDataSet(date,amount);
+	  for( int i=0 ; i<amount.size() ; i++ )
+	  {
+	     dataset.setValue(date.get(i),amount.get(i));
+	  }  
+      return dataset;         
+   }
+   
+   private static JFreeChart createChart( PieDataset dataset ) {
+      JFreeChart chart = ChartFactory.createPieChart("Expenses",dataset,true,true, false);
+      return chart;
+   }
+   
+   public static JPanel createDemoPanel( ) {
+      JFreeChart chart = createChart(createDataset( ) );
+      return new ChartPanel( chart ); 
+   }
+
+}
+
